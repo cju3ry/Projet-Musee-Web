ab744debb1de08892b7bde90a4ae486a874fcd63
qchesnier <168072017+qchesnier@users.noreply.github.com>
2024-12-05 10:18:18 +0100

    modification des chemins

--- DataBridge/index.php
+++ DataBridge/index.php
@@ -5,9 +5,9 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../outils/bootstrap-5.3.2-dist/css/bootstrap.css">
-    <link rel="stylesheet" href="../outils/fontawesome-free-6.5.1-web/css/all.css">
-    <link rel="stylesheet" href="../CSS/style.css"
+    <link rel="stylesheet" href="outils/bootstrap-5.3.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="outils/fontawesome-free-6.5.1-web/css/all.css">
+    <link rel="stylesheet" href="CSS/style.css">
     <title>DataBridge</title>
 </head>
 <body>
