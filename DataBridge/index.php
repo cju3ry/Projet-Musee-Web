cc081f68d6140170824f1db609f95d0c87c99aed
Baptiste L <baptiste.ladureau@iut-rodez.fr>
2024-12-06 08:47:41 +0100

    Ajout de la police de caractère

--- DataBridge/index.php
+++ DataBridge/index.php
@@ -3,14 +3,15 @@
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="outils/bootstrap-5.3.2-dist/css/bootstrap.css">
-    <link rel="stylesheet" href="outils/fontawesome-free-6.5.1-web/css/all.css">
-    <link rel="stylesheet" href="CSS/style.css">
-    <title>DataBridge</title>
-</head>
-<body>
-<!-- Empty body -->
-</body>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+        <link rel="stylesheet" href="outils/bootstrap-5.3.2-dist/css/bootstrap.css">
+        <link rel="stylesheet" href="outils/fontawesome-free-6.5.1-web/css/all.css">
+        <link rel="stylesheet" href="CSS/style.css">
+        <title>DataBridge</title>
+    </head>
+    <body>
+        <!-- Empty body -->
+    </body>
 </html>
