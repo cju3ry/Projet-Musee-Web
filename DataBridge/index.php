7665800c750d4b9505333b0d1a11efabca7d2821
qchesnier <168072017+qchesnier@users.noreply.github.com>
2024-12-18 10:34:19 +0100

    set de l'id de l'employe

--- DataBridge/index.php
+++ DataBridge/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include("fonction/fonction.php");
+include("fonction/fonctionsAuthentification.php");
 $login = "";
 $mdp = "";
 $tableauLoginEmploye = [];
@@ -21,6 +21,7 @@ try {
     $pdo = connecterBd('databridge');
     if($mdp != "" && $login !="") {
         $tableauLoginEmploye = loginEmploye($pdo, $login, $mdp);
+        setIdEmploye(getIdEmploye($pdo, $login));
         $tableauLoginAdmin = loginAdmin($pdo, $login, $mdp);
 
         if(count($tableauLoginEmploye) != 0) {
@@ -65,7 +66,7 @@ try {
                     </div>
                     <div class="text-center fs-2 text-bg-info py-2 rounded mt-3 d-flex align-items-center justify-content-center position-relative text-center fs-1 fw-bold text-bg-info">
                         Authentification
-                        <i class="fa-solid fa-question position-absolute end-0 top-50 translate-middle-y" title="Entrez votre identifiant puis votre mots de passe pour vous connecter" style="color: #ffffff;">&nbsp;</i>
+                        <i class="fa-solid fa-question position-absolute end-0 top-50 translate-middle-y" style="color: #ffffff;">&nbsp;</i>
                     </div>
                     <div class="mt-4">
                         <label for="Identifiant" class="form-label fs-3">Identifiant</label>
