40df4133686ad38f03082639dcfe98bc9a5c0e97
CAZOR--BONNET Adrian <adrian.cazor--bonnet@iut-rodez.fr>
2024-12-27 15:44:59 +0100

    ajout des scripts java script

--- DataBridge/javaScript/scriptBurger.js
+++ DataBridge/javaScript/scriptBurger.js
@@ -1,11 +1,51 @@
-// JavaScript pour rendre les sous-menus interactifs
+// Gestion du bouton burger pour afficher/masquer le menu offcanvas
+var burgerButton = document.querySelector('.navbar-toggler');
+var offcanvasMenu = document.querySelector('.offcanvas-start');
+
+// Lorsque l'utilisateur clique sur le bouton hamburger
+burgerButton.addEventListener('click', function() {
+    // Afficher ou masquer le menu offcanvas
+    offcanvasMenu.classList.toggle('show');
+
+    // Cacher visuellement le bouton burger quand le menu est affiché
+    if (offcanvasMenu.classList.contains('show')) {
+        burgerButton.style.visibility = 'hidden';
+    } else {
+        burgerButton.style.visibility = 'visible';
+    }
+});
+
+// Lorsque l'utilisateur ferme le menu offcanvas
+document.querySelector('.btn-close').addEventListener('click', function() {
+    offcanvasMenu.classList.remove('show');
+    burgerButton.style.visibility = 'visible';
+});
+
+// Gestion des sous-menus
 document.querySelectorAll('.submenu-icon').forEach(function(icon) {
     icon.addEventListener('click', function() {
-        var submenu = icon.closest('li').querySelector('.submenu');
-        submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block'; // Toggle visibility
+        // Trouver l'élément <ul> associé à l'icône
+        var submenu = this.closest('li').querySelector('.submenu');
+
+        // Toggle (afficher/masquer) le sous-menu
+        if (submenu.style.display === 'block') {
+            submenu.style.display = 'none';  // Masquer le sous-menu
+        } else {
+            submenu.style.display = 'block'; // Afficher le sous-menu
+        }
 
         // Rotation de l'icône
-        icon.classList.toggle('rotate');
+        this.classList.toggle('rotate');
     });
 });
 
+// Réafficher le bouton burger lorsque l'utilisateur clique en dehors du menu offcanvas
+document.addEventListener('click', function(event) {
+    // Si le clic n'est ni sur le menu offcanvas ni sur le bouton burger, on réaffiche le bouton burger
+    if (!offcanvasMenu.contains(event.target) && !burgerButton.contains(event.target)) {
+        if (offcanvasMenu.classList.contains('show')) {
+            offcanvasMenu.classList.remove('show');
+            burgerButton.style.visibility = 'visible';  // Réafficher le bouton hamburger
+        }
+    }
+});
---
+++ DataBridge/javaScript/scriptModificationEmpKo.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var updateKoModal = new bootstrap.Modal(document.getElementById("updateKoModal"));
+    updateKoModal.show();
+});
