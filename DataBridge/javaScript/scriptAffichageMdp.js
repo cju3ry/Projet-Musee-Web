406a7407bb843dfd0531b24d865f4a506af27dbb
cju3ry <168074218+cju3ry@users.noreply.github.com>
2025-01-10 10:10:17 +0100

    Supression car réorganisation en fichier

--- DataBridge/javaScript/scriptAffichageMdp.js
+++
@@ -1,9 +0,0 @@
-// Affiche ou masque le champ de mot de passe
-document.getElementById('modifierMotDePasse').addEventListener('change', function() {
-    const motDePasseContainer = document.getElementById('motDePasseContainer');
-    if (this.checked) {
-        motDePasseContainer.classList.remove('d-none'); // Affiche le champ
-    } else {
-        motDePasseContainer.classList.add('d-none'); // Masque le champ
-    }
-});
